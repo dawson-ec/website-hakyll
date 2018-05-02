4cad31ac777e66ae73c4e46a668d524847386651
Andrew Dawson <ajdawson@acm.org>
2018-05-02 15:53:46 +0100

    Enable the blog section of the site

--- site.hs
+++ site.hs
@@ -22,53 +22,53 @@ main = hakyll $ do
             >>= loadAndApplyTemplate "templates/default.html" defaultContext
             >>= relativizeUrls
 
-    -- tags <- buildTags "content/blog/*" (fromCapture "blog/tags/*.html")
+    tags <- buildTags "content/blog/*" (fromCapture "blog/tags/*.html")
 
-    -- tagsRules tags $ \ tag pattern -> do
-    --     let title = "Posts tagged \"" ++ tag ++ "\""
-    --     route   idRoute
-    --     compile $ do
-    --         posts <- recentFirst =<< loadAll pattern
-    --         let ctx = constField "title" title `mappend`
-    --                   listField "posts" postCtx (return posts) `mappend`
-    --                   defaultContext
-    --         makeItem ""
-    --             >>= loadAndApplyTemplate "templates/tag.html" ctx
-    --             >>= loadAndApplyTemplate "templates/default.html" ctx
-    --             >>= relativizeUrls
+    tagsRules tags $ \ tag pattern -> do
+        let title = "Posts tagged \"" ++ tag ++ "\""
+        route   idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll pattern
+            let ctx = constField "title" title `mappend`
+                      listField "posts" postCtx (return posts) `mappend`
+                      defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/tag.html" ctx
+                >>= loadAndApplyTemplate "templates/default.html" ctx
+                >>= relativizeUrls
 
-    -- match "content/blog/*" $ do
-    --     route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
-    --     compile $ pandocCompiler
-    --         >>= saveSnapshot "tcontent"
-    --         >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
-    --         >>= saveSnapshot "content"
-    --         >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
-    --         >>= relativizeUrls
+    match "content/blog/*" $ do
+        route $ gsubRoute "content/" (const "") `composeRoutes` setExtension "html"
+        compile $ pandocCompiler
+            >>= saveSnapshot "tcontent"
+            >>= loadAndApplyTemplate "templates/post.html"    (postCtxWithTags tags)
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/default.html" (postCtxWithTags tags)
+            >>= relativizeUrls
 
-    -- create ["blog.html"] $ do
-    --     route idRoute
-    --     compile $ do
-    --         posts <- recentFirst =<< loadAll "content/blog/*"
-    --         let archiveCtx =
-    --                 boolField "hasposts" (\ p -> length p >= 1) `mappend`
-    --                 listField "tags" defaultContext (return (collectTags tags)) `mappend`
-    --                 listField "posts" postCtx (return posts)                    `mappend`
-    --                 listField "latest" teaserCtx  (return $ take 3 posts)       `mappend`
-    --                 constField "title" "Blog"                                   `mappend`
-    --                 defaultContext
-    --         makeItem ""
-    --             >>= loadAndApplyTemplate "templates/blog.html" archiveCtx
-    --             >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-    --             >>= relativizeUrls
+    create ["blog.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "content/blog/*"
+            let archiveCtx =
+                    boolField "hasposts" (\ p -> length p >= 1) `mappend`
+                    listField "tags" defaultContext (return (collectTags tags)) `mappend`
+                    listField "posts" postCtx (return posts)                    `mappend`
+                    listField "latest" teaserCtx  (return $ take 3 posts)       `mappend`
+                    constField "title" "Blog"                                   `mappend`
+                    defaultContext
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/blog.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
 
-    -- create ["atom.xml"] $ do
-    --     route idRoute
-    --     compile $ do
-    --         let feedCtx = postCtx `mappend` bodyField "description"
-    --         posts <- fmap (take 10) . recentFirst =<<
-    --             loadAllSnapshots "content/blog/*" "content"
-    --         renderAtom myFeedConfiguration feedCtx posts
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "content/blog/*" "content"
+            renderAtom myFeedConfiguration feedCtx posts
 
     match "templates/*" $ compile templateCompiler
 
